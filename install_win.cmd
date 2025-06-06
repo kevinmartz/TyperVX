29d63322c77c51edfbb98f5981e6c023d935e08b
Sakushi <116188905+ImSakushi@users.noreply.github.com>
2025-06-06 17:08:14 +0200

    Update install_win.cmd

--- install_win.cmd
+++ install_win.cmd
@@ -19,7 +19,7 @@ if "%locale:~0,2%"=="fr" (
     set msg_complete="Installation terminée."
     set msg_open="Ouvrez Photoshop et dans le menu supérieur cliquez sur : [Fenêtre] ^> [Extensions] ^> [TypeR]"
     set msg_pause="Appuyez sur une touche pour continuer..."
-    set msg_credits="Merci beaucoup à Swirt pour TyperTools et SeanR pour ce fork."
+    set msg_credits="Merci beaucoup à Swirt pour TyperTools et SeanR & Sakushi pour ce fork."
     set msg_discord="Discord de ScanR si besoin d'aide : https://discord.com/invite/Pdmfmqk"
 ) else if "%locale:~0,2%"=="es" (
     set msg_install="La extensión de Photoshop TypeR v!EXT_VERSION! se instalará."
@@ -27,7 +27,7 @@ if "%locale:~0,2%"=="fr" (
     set msg_complete="Instalación completada."
     set msg_open="Abre Photoshop y en el menú superior haz clic en lo siguiente: [Ventana] ^> [Extensiones] ^> [TypeR]"
     set msg_pause="Presiona cualquier tecla para continuar..."
-    set msg_credits="Muchas gracias a Swirt por TyperTools y a SeanR por este fork."
+    set msg_credits="Muchas gracias a Swirt por TyperTools y a SeanR & Sakushi por este fork."
     set msg_discord="Discord de ScanR si necesitas ayuda: https://discord.com/invite/Pdmfmqk"
 ) else if "%locale:~0,2%"=="pt" (
     set msg_install="Photoshop extension TypeR v!EXT_VERSION! will be installed."
@@ -35,7 +35,7 @@ if "%locale:~0,2%"=="fr" (
     set msg_complete="Instalação concluída."
     set msg_open="Abra o Photoshop e no menu superior clique em: [Janela] ^> [Extensões] ^> [TypeR]"
     set msg_pause="Pressione qualquer tecla para continuar..."
-    set msg_credits="Muito obrigado a Swirt pelo TyperTools e a SeanR por este fork."
+    set msg_credits="Muito obrigado a Swirt pelo TyperTools e a SeanR & Sakushi por este fork."
     set msg_discord="Discord do ScanR se precisar de ajuda: https://discord.com/invite/Pdmfmqk"
 ) else (
     set msg_install="Photoshop extension TypeR v!EXT_VERSION! will be installed."
@@ -43,7 +43,7 @@ if "%locale:~0,2%"=="fr" (
     set msg_complete="Installation completed."
     set msg_open="Open Photoshop and in the upper menu click the following: [Window] ^> [Extensions] ^> [TypeR]"
     set msg_pause="Press any key to continue..."
-    set msg_credits="Many thanks to Swirt for TyperTools and SeanR for this fork."
+    set msg_credits="Many thanks to Swirt for TyperTools and SeanR & Sakushi for this fork."
     set msg_discord="ScanR's Discord if you need help: https://discord.com/invite/Pdmfmqk"
 )
 
