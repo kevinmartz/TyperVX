bb3acc62c02704d239d08d6e235bf284eeda29af
Sakushi <116188905+ImSakushi@users.noreply.github.com>
2025-06-30 10:47:03 +0200

    Allow clearing shortcuts

--- app_src/components/modal/shortCut.jsx
+++ app_src/components/modal/shortCut.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FiX } from "react-icons/fi";
 import { locale } from "../../utils";
 
 const Shortcut = (props) => {
@@ -35,11 +36,19 @@ const Shortcut = (props) => {
     e.target.value = shortCut;
   };
 
+  const clearShortcut = () => {
+    const input = document.getElementById(`shortcut_${props.index}`);
+    if (input) input.value = "";
+  };
+
   return (
     <React.Fragment key={props.index}>
       <div className="field-mini-label">{locale[`shortcut_${props.index}`]}</div>
-      <div className="field-input">
-        <input id={`shortcut_${props.index}`} value={props.value.join(" + ")} onKeyDown={changeShortCut} className="topcoat-textarea" />
+      <div className="field-input shortcut-field">
+        <input id={`shortcut_${props.index}`} defaultValue={props.value.join(" + ")} onKeyDown={changeShortCut} className="topcoat-textarea" />
+        <button type="button" className="topcoat-icon-button--large--quiet" title={locale.delete} onClick={clearShortcut}>
+          <FiX size={14} />
+        </button>
       </div>
     </React.Fragment>
   );
