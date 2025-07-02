ba5328c7f481dd161df1a876d618eaf0ea13b7c5
Sakushi <116188905+ImSakushi@users.noreply.github.com>
2025-07-02 15:51:40 +0200

    Improve diacritic offset icons

--- app_src/components/modal/editStyle.jsx
+++ app_src/components/modal/editStyle.jsx
@@ -6,6 +6,7 @@ import PropTypes from "prop-types";
 import { FiCopy, FiX, FiMinus } from "react-icons/fi";
 import { TiSortAlphabetically } from "react-icons/ti";
 import { GiVerticalFlip, GiHorizontalFlip } from "react-icons/gi";
+import { TbOutlineAlphabetArabic } from "react-icons/tb";
 import { MdDelete, MdCancel, MdSave, MdFormatColorText } from "react-icons/md";
 import { GrSubscript, GrSuperscript, GrBlockQuote, GrMonospace } from "react-icons/gr";
 import { BsTypeBold, BsTypeItalic, BsFonts, BsType, BsTypeUnderline, BsTypeStrikethrough } from "react-icons/bs";
@@ -469,15 +470,17 @@ const StyleDetails = React.memo(function StyleDetails(props) {
       {context.state.middleEast && (
         <div className="style-edit-props-row">
           <div className="style-edit-props-col">
-            <div className="style-edit-props-icon" title={locale.editStyleDiacXOffset}>
-              <GiHorizontalFlip size={24} />
+            <div className="style-edit-props-icon double" title={locale.editStyleDiacXOffset}>
+              <TbOutlineAlphabetArabic size={12} />
+              <GiHorizontalFlip size={12} />
             </div>
             <input type="number" value={textStyle.diacXOffset || 0} onChange={(e) => changeProp("diacXOffset", Number(e.target.value) || 0)} className="topcoat-text-input--large" />
             <span className="style-edit-props-unit">{unit}</span>
           </div>
           <div className="style-edit-props-col">
-            <div className="style-edit-props-icon" title={locale.editStyleMarkYOffset}>
-              <GiVerticalFlip size={24} />
+            <div className="style-edit-props-icon double" title={locale.editStyleMarkYOffset}>
+              <TbOutlineAlphabetArabic size={12} />
+              <GiVerticalFlip size={12} />
             </div>
             <input type="number" value={textStyle.markYDistFromBaseline || 100} onChange={(e) => changeProp("markYDistFromBaseline", Number(e.target.value) || 0)} className="topcoat-text-input--large" />
             <span className="style-edit-props-unit">{unit}</span>
